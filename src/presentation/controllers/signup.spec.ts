068571107ac218147b127bf66dceaa2f560ff114
luizfernando <luiz-fsilva37@hotmail.com>
2022-06-03 23:48:55 -0300

    test: ensure SignUpController return 400 if no e-mail is provided

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -16,3 +16,20 @@ describe('Signup controller', () => {
     expect(httpResponse.body).toEqual(new Error('Missing param: name'))
   })
 })
+
+describe('Signup controller', () => {
+  test('Should return 400 if no e-mail is provided.', () => {
+    const sut = new SignUpController()
+    const httpRequest = {
+      body: {
+        name: 'anyname',
+        password: 'anypassword',
+        passwordConfirmation: 'anypassword'
+      }
+    }
+
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new Error('Missing param: email'))
+  })
+})
